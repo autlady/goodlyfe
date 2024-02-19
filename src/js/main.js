32673515911cea3697029fa434704019a8ee032a
Julia Grigoreva <julia_panina@mail.ru>
2024-02-19 22:03:44 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-  
-
   /* ===== отобразить форму поиска по клику на лупу в шапке =====*/
   const headerSearchOpen = document.querySelector('#search-open');
   const headerSearchForm = document.querySelector('#header-search-form');
 
   headerSearchOpen.addEventListener('click', () => {
     headerSearchForm.classList.toggle('visible');
-  })
+  });
+  
+  const toggleMenu = document.querySelectorAll('.menu-toggle');
+  const bodyEl = document.body;
+  
+  if (toggleMenu) {
+    toggleMenu.addEventListener('click', function(){
+         
+          if(this.classList.contains('active')){
+              this.classList.remove('active');
+              // asideMenu.classList.remove('active');
+              // bodyEl.classList.remove('noscroll');
+          }else{
+              this.classList.add('active');
+              // asideMenu.classList.add('active');
+              // bodyEl.classList.add('noscroll');
+          }
+      });
+  };
+
+
+
+
 })
